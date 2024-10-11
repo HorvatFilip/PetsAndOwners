a8dbdda1453a8535b68caf85c44fa8a9be76f4fd
HorvatFilip <filip.horvat997@gmail.com>
2024-10-11 17:07:19 +0200

    added heath check for db added superuser creation for django admin portal

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -3,6 +3,9 @@
 echo "Applying database migrations..."
 python manage.py migrate
 
+echo "Creating user for admin view..."
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('db-admin', 'db-admin@example.com', 'pass') if len(User.objects.filter(username='db-admin'))==0 else print('db-admin already exist')" | python manage.py shell
+
 echo "Loading test data..."
 python manage.py loaddata /test_data/test_data.json
 
