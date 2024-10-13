ad14f24837ec5d1a89f89c13d9da196331540c92
HorvatFilip <filip.horvat997@gmail.com>
2024-10-13 18:39:51 +0200

    added shared-owners-count api

--- src/backend/PetAndOwners/pao/views/__init__.py
+++ src/backend/PetAndOwners/pao/views/__init__.py
@@ -1,3 +1,4 @@
 from .owner_view import OwnersListApiView, OwnerApiView, OwnerPetsApiView
 from .pet_view import PetsListApiView, PetApiView
 from .shop_view import ShopApiView
+from .stats_view import stats_api
--- src/backend/PetAndOwners/pao/views/stats_view.py
+++ src/backend/PetAndOwners/pao/views/stats_view.py
@@ -2,12 +2,15 @@ from ninja import NinjaAPI
 from django.db.models import Count
 from pao.models import Pet, Owner
 
-api = NinjaAPI()
+stats_api = NinjaAPI()
 
-
-@api.get("stats/same-pets")
+@stats_api.get("/shared-owners-count")
 def stats_same_pets(request):
-    pet_stats = {}
-    for pet in Pet.objects.all():
-        numof_same_owners = 0
-        if pet in 
+    pets_with_multiple_owners = Pet.objects.annotate(numof_owners=Count('owners')).filter(numof_owners__gt=1)
+    unique_owner_ids = set()
+    for pet in pets_with_multiple_owners:
+        owners = pet.owners.values_list('id', flat=True)
+        unique_owner_ids.update(owners)
+
+    return {"shared-owners-count": len(unique_owner_ids)}
+    
